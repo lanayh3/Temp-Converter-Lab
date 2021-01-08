ceec929a204af352f71f6d61663c6c1f57e70ef8
lanayh3 <74631402+lanayh3@users.noreply.github.com>
2021-01-07 21:27:14 -0500

    rewrote html, rewrote js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let finalValue = document.querySelector('.result');
-let button = document.querySelector('myBtn');
+var input = document.getElementById('inputFahrenheit').value;
+let finalValue = document.getElementById('result');
+let button = document.getElementById('myBtn');
 button.addEventListener('click', temperatureConverter);
 
 function temperatureConverter() {
-let fahrenheit = parseFloat(document.querySelector('#inputFahrenheit').value);
-    //store the value to a variable up to 2 decimal
-    let result = (fahrenheit - 32) / 1.8;
-    //add result to the page
-    finalValue.innerHTML = result;
+  document.getElementById('inputFahrenheit').value
+  valNum = parseFloat(input);
+  finalValue.innerHTML = ((document.getElementById('inputFahrenheit').value-32) / 1.8);
 }
